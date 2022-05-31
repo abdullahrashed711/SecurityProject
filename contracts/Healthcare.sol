f41037abd4f6a5c7ec4c308bde841cd698e15424
bula <abdollahali68@gmail.com>
2022-05-31 13:01:48 +0200

    created the createPatient function etc

--- contracts/Healthcare.sol
+++ contracts/Healthcare.sol
@@ -4,5 +4,28 @@ pragma solidity >=0.5.0 <0.9.0;
 contract Healthcare{
     uint public patientcount=0;
  
+ struct Patient{
+    uint id;
+    string name;
+    string age;
+    string weight;
+    string height;
+    string sex;
+    string[] initialMeasurements;
+    string[] oldVisits;
+ }
+
+ mapping(uint=>Patient) public patients; //store patients in db aka blockchain ,treat this like a model
+
+ function createPatient(string memory _name,string memory _sex, 
+    string[] memory _initialMeasurements,      //keyword memory temporarly stores data in vars
+    string[] memory _oldVisits,string memory _weight ,
+    string memory _height,string memory _age) public {
+     patientcount++;    
+
+     patients[patientcount]=Patient(patientcount,_name,_age,_weight,_height,
+     _sex,_initialMeasurements,_oldVisits);
+ } 
+
 }
 
